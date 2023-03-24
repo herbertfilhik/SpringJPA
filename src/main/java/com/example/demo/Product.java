af987ada9a04a690865fc63b2f57d23c0cdf6663
herbertfilhik <herbertfilhik@gmail.com>
2023-03-23 21:07:38 -0300

    atualizações

---
+++ src/main/java/com/example/demo/Product.java
@@ -0,0 +1,72 @@
+package com.example.demo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, name = "name")
+	private String name;
+
+	@Column(nullable = false, name = "description")
+	private String description;
+
+	@Column(nullable = false, name = "price")
+	private Double price;
+
+	public Product() {
+	}
+
+	public Product(String name, String description, Double price) {
+		this.name = name;
+		this.description = description;
+		this.price = price;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", description=" + description + ", price=" + price + "]";
+	}
+}
